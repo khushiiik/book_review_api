e58b0f7b7fc1859a6b7bede45877849784110b10
khushiiik <khushikoriya118@gmail.com>
2025-07-02 13:14:23 +0530

    Modified app.py

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 from flask import Flask
+from flask_cors import CORS
 from database import db
 from model import Book,Review
 from routes.book_routes import book_bp
 from routes.review_routes import review_bp
-from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.register_blueprint(book_bp)
-app.register_blueprint(review_bp)
+CORS(app)
 
 #database config(SQLit)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book_reviews.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 db.init_app(app)
-migrate = Migrate(app, db)
+
+app.register_blueprint(book_bp)
+app.register_blueprint(review_bp)
+
+with app.app_context():      
+    db.create_all()
 
 @app.route('/')
 def home():
     return {'message': 'Book Review API is running!'}
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    app.run()
